90a8327ef50b3bf56b1446ca332c8ccee57a1f73
KiralyBence12 <118981185+KiralyBence12@users.noreply.github.com>
2025-11-10 11:20:43 +0100

    Light-Dark

---
+++ script.js
@@ -0,0 +1,19 @@
+function switchTheme() {
+  document.body.classList.add('bg-dark')
+
+  const navbar = document.getElementById('navbar')
+  const cards = document.querySelectorAll('.card')
+
+  const tables = document.querySelectorAll('.table')
+
+  navbar.classList.remove('navbar-light', 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
+
+  cards.forEach(card => {
+    card.classList.add("text-white", "bg-dark", "border-light")
+  });
+
+  tables.forEach(table => {
+    table.classList.add('table-dark')
+  })
+}
